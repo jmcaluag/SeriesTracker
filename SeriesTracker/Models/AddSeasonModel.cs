6f2f86932da70b375df0ecf7ec76ca8206d311be
Joseph <mcal5071@gmail.com>
2020-07-10 09:18:45 +1200

    Feat: Create AddSeasonModel for Adding a Season Page

---
+++ SeriesTracker/Models/AddSeasonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriesTracker.Models
+{
+    public class AddSeasonModel
+    {
+        public string WikipediaURL { get; set; }
+        public bool OneSeason { get; set; }
+        public int SpecifiedSeason { get; set; }
+    }
+}
