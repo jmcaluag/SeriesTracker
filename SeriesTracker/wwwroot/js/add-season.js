93708075d6fd174f514e2fb651a1e28deae7a6f2
Joseph <mcal5071@gmail.com>
2020-08-14 11:46:15 +1200

    Remove name from radio input. add-season.js no longer working.

--- SeriesTracker/wwwroot/js/add-season.js
+++ SeriesTracker/wwwroot/js/add-season.js
@@ -1,11 +1,11 @@
-﻿var radioSelection = document.querySelector(".radio-form");
-var radioOptions = document.querySelectorAll('input[name="radio-season"]');
-var seasonTextBox = document.querySelector('.specified-season');
+﻿const radioSelection = document.querySelector(".radio-form");
+const radioOptions = document.querySelectorAll('input[name="OneSeason"]');
+const seasonTextBox = document.querySelector('.specified-season');
 
 function reveal() {
     for (var i = 0; i < radioOptions.length; i++) {
         if (radioOptions[i].checked) {
-            radioOptions[i].value == "true" ? seasonTextBox.classList.add('form-hide') : seasonTextBox.classList.remove('form-hide');
+            radioOptions[i].value == "false" ? seasonTextBox.classList.add('form-hide') : seasonTextBox.classList.remove('form-hide');
         }
     }
 
